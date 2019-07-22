efa04f1dc92b1063ffee5aad3dba4ea2cc8bdb83
DylanTomberlin <dylan.tomberlin@flukenetworks.com>
2019-07-22 08:14:54 -0700

    changed condition when building packet to determine if 802.3bt extension is active

--- src/daemon/protocols/lldp.c
+++ src/daemon/protocols/lldp.c
@@ -325,7 +325,8 @@ static int _lldp_send(struct lldpd *global,
 				goto toobig;
 		}
 		/* 802.3bt */
-		if(port->p_power.powerTypeExt != LLDP_DOT3_POWER_TYPE_BTOFF) {
+		/* if(port->p_power.powerTypeExt != LLDP_DOT3_POWER_TYPE_BTOFF) {*/
+		if(port->p_power.dualMode) {
 			if(!(
 			     POKE_UINT16(port->p_power.requestedA) &&
 			     POKE_UINT16(port->p_power.requestedB) &&
